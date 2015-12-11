1e7b1d8977f572348b6a8911b194ac4327d5a800
xufei <xu.fei@outlook.com>
2015-12-11 16:46:50 +0800

    shopping

--- src/modules/demo/flow/index.js
+++ src/modules/demo/flow/index.js
@@ -1,7 +1,9 @@
 import EuclidDirective from "./directives/euclid";
 import ChoiceDirective from "./directives/choice";
+import ShoppingDirective from "./directives/shopping";
 
 export default angular.module("modules.demo.flow", [])
 	.directive("snEuclid", () => new EuclidDirective())
 	.directive("snChoice", () => new ChoiceDirective())
+	.directive("snShopping", () => new ShoppingDirective())
 	.name;
